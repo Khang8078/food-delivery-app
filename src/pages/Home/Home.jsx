14addd0d24c83b97194623167cb6af1c63483c4e
Khang Huynh <khanghuynh8078@gmail.com>
2024-04-03 00:38:44 -0700

    working on header

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import { Header } from '../../components/Header/Header';
+import './Home.css';
+
+export const Home = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
