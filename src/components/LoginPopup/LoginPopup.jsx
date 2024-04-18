d0cff320fbbb8fd5a4636c5edcb7cb3438f422e9
Khang Huynh <khanghuynh8078@gmail.com>
2024-04-17 23:26:07 -0700

    done sign in

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -14,15 +14,15 @@ const LoginPopup = ({setShowLogin}) => {
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
             <div className="login-popup-inputs">
-                <div className="login-pop-inputs">
-                    {currState === "Login"?<></>:<input type='text' placeholder='Your name' required/>}
-                    <input type="email" placeholder='Your email' required />
-                    <input type="password" placeholder='Password' required />
-                </div>
-                <button>{currState==="Sign Up"?"Create account":"Login"}</button>
-                <div className='login-popup-condition'></div>
+                {currState === "Login"?<></>:<input type='text' placeholder='Your name' required/>}
+                <input type="email" placeholder='Your email' required />
+                <input type="password" placeholder='Password' required />
+            </div>
+            <button>{currState==="Sign Up"?"Create account":"Login"}</button>
+            <div className='login-popup-condition'>
                 <input type="checkbox" required />
-                <p>By continuing, i agree to the terms of use & privacy policy.</p>
+                <p>By continuing, I agree to the terms of use & privacy policy.</p>
+ 
             </div>
             {currState === "Login"
             ?<p>Create a new account? <span onClick={()=> setCurrState("Sign Up")}>Click here</span></p>
