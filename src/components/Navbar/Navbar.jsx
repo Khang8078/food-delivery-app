b4fe2c477a81adfc280172c74998540aaeb4de5b
Khang Huynh <khanghuynh8078@gmail.com>
2024-04-18 00:43:58 -0700

    Revert "make the website responsive"
    This reverts commit 512db4e2028ff6347f4b5af985cf353227282b67.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@
 import { useState } from 'react';
 import './Navbar.css'
 import {assets} from '../../assets/assets'
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'; // Import PropTypes
 
 const Navbar = ({setShowLogin}) => {
@@ -12,10 +11,10 @@ const Navbar = ({setShowLogin}) => {
     <div className='navbar'>
         <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
         <ul className="navbar-menu">
-          <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-          <a href='#explore-menu'  onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-          <a href='#app-download'  onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-          <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+          <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
+          <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+          <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+          <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
         </ul>
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
